e6430b64fbcf3e7c24dbb0e2a830c95eafb35a15
0xmu <89046951+0xmu@users.noreply.github.com>
2021-09-12 13:11:21 +0100

    stats, subgraph usage

--- api/settler.js
+++ api/settler.js
@@ -1,85 +1,35 @@
-const { ethers } = require('ethers');
-const { SETTLER_ABI, DATA } = require('../lib/constants.js');
-require('dotenv').config();
+import { getSettlingPositionIDs } from '../lib/api.js'
+import { initContract, wrapRes } from '../lib/utils.js'
 
-function returnRes(res, status, obj, isLocal) {
-	if (isLocal) {
-		console.log(status, obj);
-		return;
-	}
-	res.status(status).json(obj);
-}
+export default async function main(req, res) {
 
-async function main(req, res) {
+	const contract = initContract(true);
+	if (!contract) return wrapRes(req, res, 403, {error: 'Contract null.'});
 
-	const chain = req.query.chain; // rinkeby, mainnet, etc.
+	const ids = await getSettlingPositionIDs();
 
-	if (!chain || !DATA[chain]) return returnRes(res, 403, {success: false, message: 'chain invalid'}, req.query.isLocal);
+	if (ids.length > 0) {
 
-	const pkey = process.env[`SETTLER_PKEY_${chain.toUpperCase()}`];
-
-	if (!pkey) return returnRes(res, 403, {success: false, message: 'no pkey'}, req.query.isLocal);
-
-	const trading = process.env[`TRADING_CONTRACT_${chain.toUpperCase()}`];
-
-	console.log('trading', trading);
-	
-	if (!trading) return returnRes(res, 403, {success: false, message: 'no trading contract'}, req.query.isLocal);
-
-	const provider = new ethers.providers.JsonRpcProvider(DATA[chain]['network']);
-	const walletWithProvider = new ethers.Wallet(pkey, provider);
-
-	const contract = new ethers.Contract(trading, SETTLER_ABI, walletWithProvider);
-
-
-	// determine which IDs to settle based on recent NewPosition events
-
-	const filter_settled = contract.filters.NewPositionSettled();
-	const events_settled = await contract.queryFilter(filter_settled, -10000);
-
-	console.log('events_settled', events_settled);
-
-	let already_settled = {};
-	for (let ev of events_settled) {
-		let args = ev.args;
-		already_settled[args.positionId] = true;
-	}
-
-	const filter_new = contract.filters.NewPosition();
-	const events_new = await contract.queryFilter(filter_new, -10000);
+		const canBeSettled = await contract.canSettlePositions(ids);
 		
-	console.log('events_new', events_new);
-
-	let settle_these_ids = {};
-	for (let ev of events_new) {
-		let args = ev.args;
-		if (!already_settled[args.positionId]) settle_these_ids[args.positionId] = true;
-	}
-
-	let settleTheseIds = Object.keys(settle_these_ids);
-
-	console.log('settleTheseIds', settleTheseIds);
-	// Check if they can be settled
-	if (settleTheseIds.length > 0) {
-		const canBeSettled = await contract.canSettlePositions(settleTheseIds);
 		if (canBeSettled.length > 0) {
+		
 			// Go through it and make sure there are non zeros
 			let idsToActuallySettle = [];
 			for (const id of canBeSettled) {
 				if (!id || id.toNumber() == 0) continue;
 				idsToActuallySettle.push(id);
 			}
-			console.log('idsToActuallySettle', idsToActuallySettle);
+		
 			if (idsToActuallySettle.length > 0) {
 				await contract.settlePositions(idsToActuallySettle);
-				console.log('Settled Ids', idsToActuallySettle);
-				return returnRes(res, 200, {success: true, settled: idsToActuallySettle}, req.query.isLocal);
+				return wrapRes(req, res, 200, {success: true, settled: idsToActuallySettle});	
 			}
+		
 		}
+	
 	}
 
-	return returnRes(res, 200, {success: true}, req.query.isLocal);
+	wrapRes(req, res, 200, {success: true});
 
 }
-
-module.exports = main;
