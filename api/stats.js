fbc39edaf42194178166656c1f689527e0be3d41
0xmu <89046951+0xmu@users.noreply.github.com>
2021-12-19 19:21:20 +0400

    Updates

--- api/stats.js
+++ api/stats.js
@@ -1,6 +1,7 @@
 import { getPositions, getTrades, getPrice } from '../lib/api.js'
 import { ADDRESS_ZERO } from '../lib/constants.js'
 import { initContract, wrapRes, formatUnits, formatToDisplay } from '../lib/utils.js'
+import { Table } from 'console-table-printer'
 
 export default async function main(req, res) {
 
@@ -20,17 +21,14 @@ export default async function main(req, res) {
 	let product_prices = {}; // product => product price
 	let product_info = {}; // product id => product info
 	let total_upl = {
-		recent: 0,
-		size: 0
+		ETH: 0,
+		USDC: 0
 	};
 	let total_margin = {
-		recent: 0,
-		size: 0
+		ETH: 0,
+		USDC: 0
 	};
-	let unique_owners = {
-		recent: {},
-		size: {}
-	};
+	let unique_owners = {};
 
 	const augmentPositions = async (type) => {
 
@@ -47,6 +45,10 @@ export default async function main(req, res) {
 				product_info[p.productId] = productInfo;
 				product_prices[p.product] = price;
 			}
+
+			const _p = JSON.parse(JSON.stringify(p));
+
+			let currency = _p.currency == ADDRESS_ZERO ? 'ETH' : 'USDC';
 
 			// Calculate unrealized p/l
 
@@ -65,12 +67,12 @@ export default async function main(req, res) {
 				// Add interest
 				let now = parseInt(Date.now() / 1000);
 
-				if (now < p.timestamp * 1 + 1800) {
+				if (now < p.createdAtTimestamp * 1 + 1800) {
 					//console.log('i1');
 					interest = 0;
 				} else {
 					//console.log('i2');
-					interest = p.size * ((product_info[p.productId].interest * 1 || 0) / 10000) * (now - p.timestamp * 1) / (360 * 24 * 3600);
+					interest = p.size * ((product_info[p.productId].interest * 1 || 0) / 10000) * (now - p.createdAtTimestamp * 1) / (360 * 24 * 3600);
 				}
 
 				if (interest < 0) interest = 0;
@@ -78,21 +80,32 @@ export default async function main(req, res) {
 
 			}
 
-			p.canBeLiquidated = p.isLong && p.liquidationPrice*1 > latestPrice*1 || !p.isLong && p.liquidationPrice*1 < latestPrice*1;
+			p.canBeLiquidated = p.isLong && p.liquidationPrice*1 > latestPrice*1 || !p.isLong && p.liquidationPrice*1 < latestPrice*1 ? 'Y' : '';
 
 			p.upl = formatToDisplay(upl) * 1;
 			p.productPrice = formatToDisplay(latestPrice) * 1;
 			if (interest) p.interest = formatToDisplay(-1 * interest) * 1 || 0;
 
-			p.timestamp = new Date(p.timestamp * 1000).toLocaleString();
+			p.createdAtTimestamp = new Date(p.createdAtTimestamp * 1000).toLocaleString();
+			p.updatedAtTimestamp = new Date(p.updatedAtTimestamp * 1000).toLocaleString();
+
+			total_margin[currency] += p.margin * 1;
+			total_upl[currency] += upl;
+
+			p.margin = `${p.margin} ${currency}`;
+			p.size = `${p.size} ${currency}`;
+			p.upl = `${p.upl} ${currency}`;
+			let direction = _p.isLong == true ? '↑' : '↓';
+
+			p.product = `${direction} ${p.product}`;
 
 			delete p.productId;
+			delete p.currency;
+			delete p.isLong;
 
 			positions[type][i] = p;
 			
-			total_upl[type] += upl;
-			total_margin[type] += p.margin * 1;
-			unique_owners[type][p.owner] = 1;
+			unique_owners[p.user] = 1;
 
 			i++;
 
@@ -115,15 +128,77 @@ export default async function main(req, res) {
 
 	// console.log('Treasury Balance: ' + treasuryBalance + ' | Vault Balance: ' + vaultBalance + ' | Vault Threshold: ' + vaultThreshold);
 
-	console.log("Open Positions sorted by Size");
-	console.log("Total UPL: " + formatToDisplay(total_upl.size) + " | Total Margin: ", formatToDisplay(total_margin.size) + " | Positions: " + positions.size.length + " |  Unique Wallets: " + Object.keys(unique_owners.size).length);
-	console.table(positions.size);
+	const p = new Table({
+	  columns: [
+	    { name: 'product', title: 'Product', color: 'green' },
+	    { name: 'price', title: 'Entry Price', color: 'yellow' },
+	    { name: 'productPrice', title: 'Current Price', color: 'yellow' },
+	    { name: 'margin', title: 'Margin', color: 'yellow' },
+	    { name: 'size', title: 'Size', color: 'yellow' },
+	    { name: 'leverage', title: 'Lev', color: 'yellow' },
+	    { name: 'upl', title: 'UP/L', color: 'yellow' },
+	    { name: 'interest', title: 'Interest', color: 'yellow' },
+	    { name: 'liquidationPrice', title: 'Liq. Price', color: 'yellow' },
+	    { name: 'createdAtTimestamp', title: 'Created', color: 'green' },
+	    { name: 'updatedAtTimestamp', title: 'Updated', color: 'green' },
+	    { name: 'user', title: 'User', color: 'green' },
+	    { name: 'canBeLiquidated', title: 'Can Liq.', color: 'green' }
+	  ],
+	});
 
+	p.addRows(positions.size);
+
+	console.log("Open Positions sorted by Size");
+	console.log("Total UPL: " + formatToDisplay(total_upl.ETH) + " ETH, " + formatToDisplay(total_upl.USDC) + " USDC | Total Margin: ", formatToDisplay(total_margin.ETH) + " ETH, " + formatToDisplay(total_margin.USDC) + " USDC | Positions: " + positions.size.length + " |  Unique Wallets: " + Object.keys(unique_owners).length);
+	// console.table(positions.size);
+	p.printTable();
+
+	const p2 = new Table({
+	  columns: [
+	    { name: 'product', title: 'Product', color: 'green' },
+	    { name: 'price', title: 'Entry Price', color: 'yellow' },
+	    { name: 'productPrice', title: 'Current Price', color: 'yellow' },
+	    { name: 'margin', title: 'Margin', color: 'yellow' },
+	    { name: 'size', title: 'Size', color: 'yellow' },
+	    { name: 'leverage', title: 'Lev', color: 'yellow' },
+	    { name: 'upl', title: 'UP/L', color: 'yellow' },
+	    { name: 'interest', title: 'Interest', color: 'yellow' },
+	    { name: 'liquidationPrice', title: 'Liq. Price', color: 'yellow' },
+	    { name: 'createdAtTimestamp', title: 'Created', color: 'green' },
+	    { name: 'updatedAtTimestamp', title: 'Updated', color: 'green' },
+	    { name: 'user', title: 'User', color: 'green' },
+	    { name: 'canBeLiquidated', title: 'Can Liq.', color: 'green' }
+	  ],
+	});
+
+	p2.addRows(positions.recent);
 
 	console.log("Recent Positions");
-	console.table(positions.recent);
+	p2.printTable();
+
+	// console.table(positions.recent);
+
+	const p3 = new Table({
+	  columns: [
+	    { name: 'isLong', title: 'Direction', color: 'green' },
+	    { name: 'product', title: 'Product', color: 'green' },
+	    { name: 'entryPrice', title: 'Entry Price', color: 'yellow' },
+	    { name: 'closePrice', title: 'Close Price', color: 'yellow' },
+	    { name: 'margin', title: 'Margin', color: 'yellow' },
+	    { name: 'size', title: 'Size', color: 'yellow' },
+	    { name: 'leverage', title: 'Lev', color: 'yellow' },
+	    { name: 'pnl', title: 'P/L', color: 'yellow' },
+	    { name: 'duration', title: 'Duration', color: 'yellow' },
+	    { name: 'timestamp', title: 'Closed At', color: 'green' },
+	    { name: 'user', title: 'User', color: 'green' },
+	    { name: 'wasLiquidated', title: 'Was Liq.', color: 'green' },
+	    { name: 'currency', title: 'Currency', color: 'green' }
+	  ],
+	});
+
+	p3.addRows(trades);
 
 	console.log("Trades");
-	console.table(trades);
+	p3.printTable();
 
 }
